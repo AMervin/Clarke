98d2e3c56415b388857395ab5877f814163b86bb
Addias Mervin <acmervin@gmail.com>
2011-08-31 12:29:43 -0400

    added the files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clarke::Application.config.secret_token = '5238c170131563f4d31a881e9b1de4791664913c7960a1f091e435c3a4fa5ae55d4b9e6d30b8a4214197e8ed409e2d056074a12d7549761bf5abe9c0ed479cb2'
